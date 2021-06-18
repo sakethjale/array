aee384335ad6a9d88c3bccff68c4c2dc9e23bd58
sakethjale <84376881+sakethjale@users.noreply.github.com>
2021-06-18 18:20:54 +0530

    Add files via upload

---
+++ divisions.js
@@ -0,0 +1,19 @@
+class Divisions {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
---
+++ sketch.js
@@ -0,0 +1,77 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //create division objects
+  for (var k = 0; k <=800; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //create 1st row of plinko objects
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //create 3rd row of plinko objects
+for(var j=25;j<=width-10;j=j+50 )
+{
+   plinkos.push(new Plinko(j,275))
+}
+  
+  //create 4th row of plinko objects
+for(var j=25;j<=width-10;j=j+50)
+{
+  plinkos.push(new Plinko(j,375))
+}
+
+  //create particle objects
+ 
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  //display the paricles 
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/1-40, width/8+100), 10,10));
+  }
+  //we can keep any vairable but we should write it in  setup also
+  
+  for (var k = 0; k < particles.length; k++) {
+    particles[k].display();
+}
+}
